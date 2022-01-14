dfa3176ef5efc4efe04eb09289534a8365c56d2c
Priya <7401priya@gmail.com>
2022-01-14 13:29:02 +0530

    initial commit

---
+++ Meme-generator/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <Meme/>
+    </div>
+  );
+}
